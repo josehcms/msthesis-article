9321e5ded15f4d635e7cdd040718c34267d6543c
josehcms <zecosta.monteiro@gmail.com>
2020-07-09 22:53:43 -0300

    update scripts, rmarkdown and figs

--- R/supmat1_data_analysis.R
+++ R/supmat1_data_analysis.R
@@ -246,42 +246,119 @@ require(ggplot2)
       by = c( 'reg', 'educ.fem', 'educ.mal' ) 
     )
   
+  dic_educ <- c( '1' = 'LP', '2' = 'P', '3' = 'S', '4' = 'T' )
+  
+  tab1 <- 
+    dcastdat.c15[ , 
+                  list(
+                    reg, 
+                    educ.fem = dic_educ[ paste0( educ.fem ) ],
+                    educ.mal = dic_educ[ paste0( educ.mal ) ],
+                    prevc1 = format( prevc1, nsmall = 2 ), 
+                    prevc2 = format( prevc2, nsmall = 2 ),
+                    prevc3 = format( prevc3, nsmall = 2 ),
+                    cfrc1  = format( round( cfrc1, 2 ), nsmall = 2 ),
+                    cfrc2  = format( round( cfrc2, 2 ), nsmall = 2 ),
+                    cfrc3  = format( round( cfrc3, 2 ), nsmall = 2 ) 
+                  )]
+  
+  write.csv2( tab1, 'FIGS/prevcfr_tab_data.csv', row.names = F )
   # compute standardized cfr by educ pairing prevalence in the cohort (R) 
   # and the standirdized prevalence by cfr distribution (I)
   
-  dcastdat.c15[,
-               `:=`(
-                 R1 = cfrc1  * ( prevc1 / 100 + prevc2 / 100 ) / 2,
-                 R2 = cfrc2  * ( prevc1 / 100 + prevc2 / 100 ) / 2,
-                 I1 = prevc1 / 100 * ( cfrc1 + cfrc2 ) / 2,
-                 I2 = prevc2 / 100 * ( cfrc1 + cfrc2 ) / 2,
-                 R3 = cfrc2  * ( prevc2 / 100 + prevc3 / 100 ) / 2,
-                 R4 = cfrc3  * ( prevc2 / 100 + prevc3 / 100 ) / 2,
-                 I3 = prevc2 / 100 * ( cfrc2 + cfrc3 ) / 2,
-                 I4 = prevc3 / 100 * ( cfrc2 + cfrc3 ) / 2
-                 )
-               ]
+  cfr_decomp <- function( CFR1, P1, CFR2, P2 ){
+    
+    # overall CFR is a function of CFR_i and P_i: F = f(CFR_i,P_i)
+    # compute overall CFR for times 1 (F1) and 2 (F2)
+    P1 = P1 / 100
+    P2 = P2 / 100
+    
+    F1 <- sum( CFR1 * P1 )
+    F2 <- sum( CFR2 * P2 )
+    deltaF1F2 <- round( F1 - F2, 2 )
+    
+    # compute P standardized values of functions for times 1 and 2
+    F1_Pstd <- ( sum( CFR1 * P1 ) + sum( CFR1 * P2 ) ) / 2
+    F2_Pstd <- ( sum( CFR2 * P1 ) + sum( CFR2 * P2 ) ) / 2
+    
+    # compute CFR standardized values of functions for times 1 and 2
+    F1_CFRstd <- ( sum( CFR1 * P1 ) + sum( CFR2 * P1 ) ) / 2
+    F2_CFRstd <- ( sum( CFR1 * P2 ) + sum( CFR2 * P2 ) ) / 2
+    
+    # compute effects
+    CFR_effect <- round( F1_Pstd - F2_Pstd, 2 )
+    P_effect <- round( F1_CFRstd - F2_CFRstd, 2 )
+    
+    tab <- 
+      data.table(
+        effect = c( 'Overall difference', 'CFR effect', 'Educational pairing composition effect' ),
+        diff   = c( paste0( deltaF1F2, ' (', format( round( 100 * ( deltaF1F2 / deltaF1F2 ) , 2 ), nsmall = 2 ), '%)' ),
+                    paste0( CFR_effect, ' (', format( round( 100 * ( CFR_effect / deltaF1F2 ) , 2 ), nsmall = 2 ), '%)'  ), 
+                    paste0( P_effect, ' (', format( round( 100 * ( P_effect / deltaF1F2 ) , 2 ), nsmall = 2 ), '%)' ) )
+      )
+    
+    return( tab )
+    
+  }
   
-  dcastdat.c15[,
-               list(
-                 diff.c1c2    = (sum(R1)-sum(R2)+sum(I1)-sum(I2)),
-                 preveff.c1c2 = (sum(I1)-sum(I2)) / (sum(R1)-sum(R2)+sum(I1)-sum(I2)),
-                 cfreff.c1c2  = (sum(R1)-sum(R2)) / (sum(R1)-sum(R2)+sum(I1)-sum(I2)),
-                 
-                 diff.c2c3    = (sum(R2)-sum(R3)+sum(I2)-sum(I3)),
-                 preveff.c2c3 = (sum(I2)-sum(I3)) / (sum(R2)-sum(R3)+sum(I2)-sum(I3)),
-                 cfreff.c2c3  = (sum(R2)-sum(R3)) / (sum(R2)-sum(R3)+sum(I2)-sum(I3))
-               ),
-               .(reg)
-               ]
+  preptab <- function( data, regname ){
+    
+    CFR1 = data[ reg == regname ]$cfrc1
+    CFR2 = data[ reg == regname ]$cfrc2
+    CFR3 = data[ reg == regname ]$cfrc3
+    
+    P1 = data[ reg == regname ]$prevc1
+    P2 = data[ reg == regname ]$prevc2
+    P3 = data[ reg == regname ]$prevc3
+    
+    # overall CFRs
+    OCFR1 = format( round( sum( CFR1 * P1 / 100 ), 2 ), nsmall = 2 )
+    OCFR2 = format( round( sum( CFR2 * P2 / 100 ), 2 ), nsmall = 2 )
+    OCFR3 = format( round( sum( CFR3 * P3 / 100 ), 2 ), nsmall = 2 )
+    
+    tab <- 
+      rbind(
+        data.table(
+          effect = c('','',''),
+          diff   = c(OCFR1,OCFR2,OCFR3),
+          info   = c('CFR(1925-39)','CFR(1940-54)','CFR(1955-69)')
+        ),
+        cfr_decomp( CFR1 = CFR1, CFR2 = CFR2, P1 = P1, P2 = P2 ) %>%
+          .[, info := c( 'CFR(1925-39) - CFR(1940-54)', 
+                         'CFR effect1', 
+                         'Pairing composition effect1' ) ],
+        cfr_decomp( CFR1 = CFR1, CFR2 = CFR3, P1 = P1, P2 = P3 ) %>%
+          .[, info := c( 'CFR(1925-39) - CFR(1955-69)', 
+                         'CFR effect2', 
+                         'Pairing composition effect2' ) ],
+        cfr_decomp( CFR1 = CFR2, CFR2 = CFR3, P1 = P2, P2 = P3 ) %>%
+          .[, info := c( 'CFR(1940-54) - CFR(1955-69)', 
+                         'CFR effect3', 
+                         'Pairing composition effect3' ) ] ) %>%
+      .[, .( info, diff, reg = regname ) ]
+    
+    return(tab)
+  }
   
-  brazil.decomp <- 
-    dcastdat.c15[reg=='Brazil'] %>%
-    copy %>%
-    .[,`:=`(
-      cfreff.c1c3 = round(100*(R1 - R3)/ (sum(R1)-sum(R3)+sum(I1)-sum(I3)),2),
-      preveff.c1c3 = round(100*(I1 - I3)/ (sum(R1)-sum(R3)+sum(I1)-sum(I3)),2)
-    )]
+  
+  tab2 <- 
+    rbind(
+      preptab( data = dcastdat.c15, regname = 'Brazil' ),
+      preptab( data = dcastdat.c15, regname = 'Central-West' ),
+      preptab( data = dcastdat.c15, regname = 'North-Northeast' ),
+      preptab( data = dcastdat.c15, regname = 'South-Southeast' ) 
+    ) %>% 
+    dcast( info ~ reg, value.var = 'diff' ) %>%
+    .[, info := factor( info,
+                        levels = c( "CFR(1925-39)", "CFR(1940-54)", "CFR(1955-69)",
+                                    "CFR(1925-39) - CFR(1940-54)", "CFR effect1", "Pairing composition effect1",
+                                    "CFR(1925-39) - CFR(1955-69)", "CFR effect2", "Pairing composition effect2",
+                                    "CFR(1940-54) - CFR(1955-69)", "CFR effect3", "Pairing composition effect3" ) )] %>%
+    setorder( info )
+  
+  
+  write.csv2( tab2, 'FIGS/decomp_tab_data.csv', row.names = F )
+  
 ##################################################################
 
 ### 5. CFR plots #-----------------------------------------------
@@ -333,7 +410,7 @@ require(ggplot2)
         geom_hline(
           yintercept = 2.1,
           size = 0.5,
-          color = 'red',
+          color = 'forestgreen',
           linetype = 'dashed'
           ) +
         facet_grid( 
@@ -349,30 +426,31 @@ require(ggplot2)
           ) +
         scale_color_manual( 
           labels = c( "Less than Primary", "Primary", "Secondary", "Tertiary" ), 
-          values = c( "black", "gray35", "gray55", "gray75" ),
-          name = "Male Educational Attainment Level" 
+          values = c( "black", "gray35", "tomato3", "navyblue" ),
+          name = "Male Educational\nAttainment Level" 
           ) +
         scale_linetype_manual( 
           labels = c( "Less than Primary", "Primary", "Secondary", "Tertiary" ), 
           values = c( "solid", "longdash", "dashed", "dotted" ),
-          name = "Male Educational Attainment Level" 
+          name = "Male Educational\nAttainment Level" 
         ) +
         theme_bw() +
         theme(
-          legend.position  = "bottom",
-          axis.text.y      = element_text( size = 15, color = 'black'  ),
-          axis.text.x      = element_text( size = 15, angle = 90, vjust = 0.5, color = 'black' ),
-          axis.title       = element_text( size = 16 ),
-          plot.title       = element_text( size = 22 ),
-          plot.caption     = element_text( size = 16 ),
-          plot.subtitle    = element_text( size = 28 ),
-          legend.title     = element_text( size = 16 ),
-          legend.text      = element_text( size = 15 ),
-          strip.text.x     = element_text( size = 15 ),
-          strip.text.y     = element_text( size = 14 ),
-          panel.grid.major = element_line( colour = "gray80", size = 0.25, linetype = 'dashed' ),
-          panel.grid.minor = element_line( colour = "gray90", size = 0.15, linetype = 'dotted'  ),
-          legend.direction = 'vertical'
+          legend.position  = "top",
+          axis.text.y      = element_text( size = 14, color = 'black'  ),
+          axis.text.x      = element_text( size = 14, angle = 90, vjust = 0.5, color = 'black' ),
+          axis.title       = element_text( size = 16, color = 'black', face = 'bold' ),
+          plot.title       = element_text( size = 22, color = 'black'  ),
+          plot.caption     = element_text( size = 16, color = 'black'  ),
+          plot.subtitle    = element_text( size = 28, color = 'black'  ),
+          legend.title     = element_text( size = 15, color = 'black', face = 'bold' ),
+          legend.text      = element_text( size = 14, color = 'black'  ),
+          strip.text       = element_text( size = 14, color = 'black', face = 'bold'   ),
+          strip.background = element_rect( fill = 'gray95', color = 'gray30' ),
+          panel.grid.major = element_line( colour = "gray80", size = 0.15, linetype = 'dashed' ),
+          panel.grid.minor = element_line( colour = "gray90", size = 0.05, linetype = 'dashed'  ),
+          legend.direction = 'horizontal',
+          legend.background = element_rect( color = 'black', fill = 'transparent', size = 0.21 )
           ) +
         scale_y_continuous( 
           breaks = seq( 1, 15, 1 ),
@@ -384,9 +462,107 @@ require(ggplot2)
         ) 
     }
   
-  x11(width = 10, height = 12)
-  cfr.plot(cfrdatc5.plot)
+  # 5.2 save figure and plot data of CFR trends by ed pairing
+  cfr.plot( cfrdatc5.plot )
+  write.csv2( cfrdatc5.plot, 'FIGS/plot_cfr_5year_cohort_data.csv', row.names = F )
+  ggsave( 'FIGS/plot_cfr_5year_cohort.png', dpi = 300, width = 9, height = 9 )
+  
+  # 5.3 prepare data for CFR diff
+  rel_diffplot <- 
+    dcast( cfrdatc5.plot[!is.na(CFR)],
+           reg+cohort_5+educ.fem~educ.mal, value.var = 'CFR')
 
+  rel_diffplot[, rel_diff_LPP := ( `Less Than Primary` - Primary ) / Primary ]
+  rel_diffplot[, rel_diff_SP := ( Secondary - Primary ) / Primary ]
+  
+  rel_diffplot <- 
+    rel_diffplot %>%
+    melt(
+      id.vars = c('reg','cohort_5','educ.fem'),
+      measure.vars = c('rel_diff_LPP','rel_diff_SP'),
+      value.name = 'rel_diff',
+      variable.name = 'rel_diff_type'
+    )
+  
+  rel_diffplot[ , rel_diff_type := ifelse( rel_diff_type == 'rel_diff_LPP',
+                                          'Male Relative Difference\nLess than Primary vs Primary',
+                                          'Male Relative Difference\nSecondary vs Primary' ) ]
+  rel_diffplot[ , educ.fem := ifelse( educ.fem == 'Female: Less Than Primary',
+                                      'Female;\nLess Than Primary', educ.fem )]
+  cfr_reldiff.plot <- 
+    function( dat.c5 ){
+      ggplot(
+        data = dat.c5 %>% copy, 
+        aes( 
+          x = cohort_5,
+          y = rel_diff,
+          color    = as.factor( reg ),
+          linetype = as.factor( reg ),
+          group    = as.factor( reg )
+        )
+      ) +
+        geom_line(
+          size = 1.5
+        ) +
+        geom_hline(
+          yintercept = 0,
+          size = 0.5,
+          color = 'forestgreen',
+          linetype = 'dashed'
+        ) +
+        facet_grid( 
+          educ.fem ~ rel_diff_type 
+        ) + 
+        labs( 
+          # title = paste0( "Cohort Fertility Rates by Educational Pairing of Couples - "), 
+          # subtitle = "Women Born in 1925-1969, five-year interval",
+          # caption = "Source: IPUMS-International",
+          x = "\nFemale Cohort Year of Birth", 
+          y = "CFR relative difference\n(for male educational attainment level)", 
+          color = ""
+        ) +
+        scale_color_manual( 
+          labels = c( "Brazil", "Central-West", "North-Northeast", "South-Southeast" ), 
+          values = c( "black", "gray55", "tomato2", "steelblue4" ),
+          name = "" 
+        ) +
+        scale_linetype_manual( 
+          labels = c( "Brazil", "Central-West", "North-Northeast", "South-Southeast" ), 
+          values = c( "solid", "longdash", "dashed", "dotted" ),
+          name = "" 
+        ) +
+        theme_bw() +
+        theme(
+          legend.position  = "top",
+          axis.text.y      = element_text( size = 14, color = 'black'  ),
+          axis.text.x      = element_text( size = 14, angle = 90, vjust = 0.5, color = 'black' ),
+          axis.title       = element_text( size = 16, color = 'black', face = 'bold' ),
+          plot.title       = element_text( size = 22, color = 'black'  ),
+          plot.caption     = element_text( size = 16, color = 'black'  ),
+          plot.subtitle    = element_text( size = 28, color = 'black'  ),
+          legend.title     = element_text( size = 15, color = 'black', face = 'bold' ),
+          legend.text      = element_text( size = 14, color = 'black'  ),
+          strip.text       = element_text( size = 14, color = 'black', face = 'bold'   ),
+          strip.background = element_rect( fill = 'gray95', color = 'gray30' ),
+          panel.grid.major = element_line( colour = "gray80", size = 0.15, linetype = 'dashed' ),
+          panel.grid.minor = element_line( colour = "gray90", size = 0.05, linetype = 'dashed'  ),
+          legend.direction = 'horizontal',
+          legend.background = element_rect( color = 'black', fill = 'transparent', size = 0.21 )
+        ) +
+        scale_y_continuous( 
+          breaks = seq( - 0.80, 0.80, 0.10 ),
+          limits = c( - 0.20, 0.60 )
+        ) +
+        scale_x_continuous( 
+          breaks = seq( 1925, 1965, 5 ),
+          limits = c( 1925, 1965 )
+        ) 
+    }
+  
+  # 5.4 rel_diff plot
+  cfr_reldiff.plot( rel_diffplot )
+  write.csv2( rel_diffplot, 'FIGS/plot_reldiffcfr_5year_cohort_data.csv', row.names = F )
+  ggsave( 'FIGS/plot_reldiffcfr_5year_cohort.png', dpi = 300, width = 9, height = 9 )
   
 ##################################################################
   
